2789572113420db7db1a99b66075dd8ed5dbfa4f
zym <zym@soundbus.cn>
2020-05-19 17:11:45 +0800

    角色用户访问权限控制

--- main.go
+++ main.go
@@ -10,9 +10,12 @@ func main() {
     Demo();
     // 简单用户访问权限控制
     Simple();
+    // 角色用户访问权限控制
+    Role();
 }
 
 func Demo() {
+    fmt.Println("-------------------- 最基础demo --------------------")
     // 通过策略文件和模型配置穿件一个casbin访问控制实例
     e, _ := casbin.NewEnforcer("./config/perm.conf", "./config/policy.csv")
 
@@ -30,10 +33,10 @@ func Demo() {
             }
         }
     }
-    fmt.Println("-------------------- 华丽分隔线 --------------------")
 }
 
 func Simple() {
+    fmt.Println("-------------------- 简单用户访问权限控制 --------------------")
     //通过策略文件和模型配置穿件一个casbin访问控制实例
     e, _ := casbin.NewEnforcer("./config/simple_perm.conf", "./config/simple_policy.csv")
 
@@ -51,5 +54,25 @@ func Simple() {
             }
         }
     }
-    fmt.Println("-------------------- 华丽分隔线 --------------------")
+}
+
+func Role() {
+    fmt.Println("-------------------- 角色用户访问权限控制 --------------------")
+    //通过策略文件和模型配置穿件一个casbin访问控制实例
+    e, _ := casbin.NewEnforcer("./config/role_perm.conf", "./config/role_policy.csv")
+
+    //定义各种sub，obj和act的数组
+    subs := []string{"cjc", "zym"}
+    objs := []string{"/cust/GetUser", "/cust/CreateUser", "/content/CreateContent", "/content/GetContent"}
+    acts := []string{"read", "write"}
+
+    //遍历组合sub，obj，act并打印出对应策略匹配结果。
+    for _, sub := range subs {
+        for _, obj := range objs {
+            for _, act := range acts {
+                result, _ :=  e.Enforce(sub, obj, act)
+                fmt.Println(sub, ",", obj, ",", act, "=", result)
+            }
+        }
+    }
 }
